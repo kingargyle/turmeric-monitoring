b0736974a0d86c1bae3d4dfedcbf5627d818c71e
josealvarezmuguerza <muguerza@intalio.com>
2011-10-04 15:36:56 -0300

    TURMERIC-1527 - adds getErrorGraph method

--- turmeric-monitoring-cassandra-provider/src/main/java/org/ebayopensource/turmeric/monitoring/provider/dao/impl/BaseMetricsErrorsByFiltersDAOImpl.java
+++ turmeric-monitoring-cassandra-provider/src/main/java/org/ebayopensource/turmeric/monitoring/provider/dao/impl/BaseMetricsErrorsByFiltersDAOImpl.java
@@ -12,10 +12,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.ebayopensource.turmeric.monitoring.provider.dao.BaseMetricsErrorsByFilterDAO;
 import org.ebayopensource.turmeric.monitoring.provider.dao.MetricsErrorValuesDAO;
+import org.ebayopensource.turmeric.monitoring.provider.model.Model;
 import org.ebayopensource.turmeric.monitoring.provider.util.KeyGeneratorUtil;
 import org.ebayopensource.turmeric.runtime.error.cassandra.model.ErrorValue;
 import org.ebayopensource.turmeric.utils.cassandra.dao.AbstractColumnFamilyDao;
@@ -24,14 +26,14 @@ import org.ebayopensource.turmeric.utils.cassandra.dao.AbstractColumnFamilyDao;
  * The abstract class  BaseMetricsErrorsByFiltersDAO.
  * @author jose alvarez muguerza
  */
-public  class BaseMetricsErrorsByFiltersDAOImpl<K>  extends AbstractColumnFamilyDao<K, String>
+public  class BaseMetricsErrorsByFiltersDAOImpl<K>  extends AbstractColumnFamilyDao<K, Model>
 	implements	BaseMetricsErrorsByFilterDAO<K> {
 
     private final MetricsErrorValuesDAO errorValuesDaoImpl ;
 
 	public BaseMetricsErrorsByFiltersDAOImpl(final String clusterName, final String host,
 			final String s_keyspace,  final String columnFamilyName,final Class<K> kTypeClass,  final MetricsErrorValuesDAO errorValuesDaoImpl) {
-		super(clusterName, host, s_keyspace, kTypeClass, String.class, columnFamilyName);
+		super(clusterName, host, s_keyspace, kTypeClass, Model.class, columnFamilyName);
 		this.errorValuesDaoImpl = errorValuesDaoImpl;
 	}
 	
@@ -44,21 +46,24 @@ public  class BaseMetricsErrorsByFiltersDAOImpl<K>  extends AbstractColumnFamily
 			Map<String, List<String>> filters) {
 		// errorId comes null
 		// agregationpPeriod not used
-		// IP or ServerName for improvement, for now we just use ALL-
+		// IP or ServerName for improvement, for now we just use All-
 
 		final List<K> errorKeys =  (List<K>) KeyGeneratorUtil.generateErrorValuesKeys( serverSide,
 				filters, filter);
-		Set<K> errorValuesIds = (Set<K>) findItems(errorKeys, String.valueOf(beginTime), String.valueOf(endTime));
-		Set<ErrorValue> errorValues = errorValuesDaoImpl.findItems(
-				new ArrayList<K>(errorValuesIds), "", "");
+		Map<K, Map<Long, String>> findItems = findItems(errorKeys, beginTime, endTime);
 
 		List<Map<K, Object>> result = new ArrayList<Map<K, Object>>();
-		for (ErrorValue errorValue : errorValues) {
-			Map<K, Object> row = new HashMap<K, Object>();
-			row.put((K)"timeStamp", errorValue.getTimeStamp());
-			result.add(row);
+		Set<Entry<K, Map<Long, String>>> entrySet = findItems.entrySet();
+		for (Entry<K, Map<Long, String>> findItemSet : entrySet) {
+			Map<Long, String> value = findItemSet.getValue();
+			Set<Entry<Long, String>> entrySet2 = value.entrySet();
+			for (Entry<Long, String> findItemSet2 : entrySet2) {
+				Map<K, Object> row = new HashMap<K, Object>();
+				row.put((K)"timeStamp", findItemSet2.getKey());
+				result.add(row);
+			}
 		}
-
+	
 		return result;	
 		
 	}
